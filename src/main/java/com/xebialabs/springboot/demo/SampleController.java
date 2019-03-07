712e4ea4ef0f918f6a387d02a02a732ec1b73684
Wai Lee <wlee@xebialabs.com>
2019-03-07 13:31:22 -0500

    Update src

---
+++ src/main/java/com/xebialabs/springboot/demo/SampleController.java
@@ -0,0 +1,17 @@
+package com.xebialabs.springboot.demo;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    @RequestMapping("/help")
+    @ResponseBody
+    String home() {
+        String input = "Hello Spring Boot!";
+        return input;
+    }
+}
