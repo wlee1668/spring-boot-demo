69e8ead4b52e51f71d1165e2a6f63ec01f4cb269
Lee <Wai.Lee@vantiv.com>
2020-10-30 18:41:09 -0400

    update

--- src/main/java/com/xebialabs/springboot/demo/SampleController.java
+++ src/main/java/com/xebialabs/springboot/demo/SampleController.java
@@ -11,7 +11,7 @@ public class SampleController {
     @RequestMapping("/help")
     @ResponseBody
     String home() {
-        String input = "Hello Spring Boot!!!";
+        String input = "Hello Spring Boot!!!!!";
         return input;
     }
 }
